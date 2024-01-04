025981af45453f80adc7e02cd49878615b066116
white <sshevczo1993@gmail.com>
2024-01-04 15:44:26 +0200

    Внесены изменения в main.py

--- main.py
+++ main.py
@@ -1,47 +1,45 @@
 import requests
+from typing import List, Dict, Any
 
-class WeatherClient:
+class OpenWeatherMapClient:
     base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
-    def get_weather(self, city):
+    def get_weather(self, city: str) -> Dict[str, Any]:
         endpoint = f'{self.base_url}?q={city}&appid={self.api_key}'
         response = requests.get(endpoint)
         return response.json()
-    
+
 class WeatherStorageService:
-    def __init__(self):
-        self.wheather_result = []
+    def __init__(self) -> None:
+        self.weather_results: List[Dict[str, Any]] = []
 
-    def save_result(self, result):
-        self.wheather_result.append(result)
+    def save_result(self, result: Dict[str, Any]) -> None:
+        self.weather_results.append(result)
 
-    def get_result(self):
-        return self.wheather_result
-    
-    def delete_result(self, index):
-        if 0 <= index < len(self.wheather_result):
-            del self.wheather_result[index]
- 
+    def get_results(self) -> List[Dict[str, Any]]:
+        return self.weather_results
 
-
+    def delete_result(self, index: int) -> None:
+        if 0 <= index < len(self.weather_results):
+            del self.weather_results[index]
 
 if __name__ == '__main__':
     api_key = '1cd6c4ed54ede92936b19e15750a0cb8'
 
-    weather_client = WeatherClient(api_key)
-    city_to_check = 'Kharkov'
+    weather_client = OpenWeatherMapClient(api_key)
+    city_to_check = 'Kharkiv'
     weather_result = weather_client.get_weather(city_to_check)
     print('Weather Result:', weather_result)
 
     storage_service = WeatherStorageService()
     storage_service.save_result(weather_result)
-    saved_results = storage_service.get_result()
+    saved_results = storage_service.get_results()
     print('Saved Results:', saved_results)
 
     index_to_delete = 0
     storage_service.delete_result(index_to_delete)
-    updated_results = storage_service.get_result()
+    updated_results = storage_service.get_results()
     print('Updated Results:', updated_results)
